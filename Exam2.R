f5b16a58b53c49fd2f4310db17f43ceafb6bf69d
“gavinpaulfaulk” <gavinpaulfaulk@gmail.com>
2021-04-07 18:26:59 -0500

    extracted year from date and created separate column

---
+++ Exam2.R
@@ -0,0 +1,14 @@
+#extract year from date
+e <- read.csv(file = "Alaska_Bald_Eagle_Nest_Sites_(USFWS).csv", header = T, stringsAsFactors = F)
+
+library("stringr")
+
+year <- str_sub(string = e$modayyr, start = 1, end = 4)
+str(year)
+head(year)
+
+year <- as.numeric(year)
+e$"Year" <- year
+write.csv(x=e, file = "Eagle_Nest_year.csv")
+
+str(e)
